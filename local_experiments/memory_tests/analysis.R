03e8ac8c910fe8212e2820d1baefe48855e77850
Paulius Dilkas <paulius.dilkas@gmail.com>
2019-08-23 12:08:13 +0100

    Integrate the DAG stuff into other sections

--- local_experiments/memory_tests/analysis.R
+++ local_experiments/memory_tests/analysis.R
@@ -28,5 +28,8 @@ for (i in 0:8) {
   s = 2 ^ i
   abline(regression3$coefficients[1] + regression3$coefficients[2] * s, 1, untf = TRUE, col = i + 1)
 }
-plot(string, remaining_memory, log = "xy")
-abline(regression3$coefficients[1], regression3$coefficients[2], untf = TRUE)
+
+library(ggplot2)
+df <- data.frame(string = string, remaining_memory = remaining_memory)
+ggplot(df, aes(string, remaining_memory)) + geom_jitter(width = 0.4) + geom_smooth(method = 'lm') +
+  coord_trans(x = "log10", y = "log10") + xlab("string length") + ylab("remaining memory (MiB)")
